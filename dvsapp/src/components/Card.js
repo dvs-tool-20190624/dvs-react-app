8466114a1c54bf6deb357d4fe2102404395e1291
Jason Belgard <belgard.jason@gmail.com>
2019-06-26 22:31:37 -0500

    update card with error getting stuck

--- dvsapp/src/components/Card.js
+++ dvsapp/src/components/Card.js
@@ -1,11 +1,33 @@
-import React from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
-const Card = props => {
+class Card extends Component {
+    constructor() {
+        super();
+        this.state = {
+            budget: ''
+        };
+    }
+    handleAdd = event => {
+        event.preventDefault();
+        this.props.addAmount({ value: this.state.budget, completed: false});
+        this.setState({ budget: '' });
+    };
+    render() {
     return (
         <div className = 'card'>
-            <div className = 'fieldName'>{props.card.headline}</div>
-            <div className = 'fieldAmount'></div>
+            <div className = 'fieldName'>{props.card.fieldName}</div>
+            <div className = 'fieldAmount'>
+                <form onSubmit = {this.handleAdd}>
+                    <input
+                        onChange = {event => this.setState({ budget: event.target.value})}
+                        name = 'amount'
+                        value = {this.state.budget}
+                        placeholder = 'Enter the amount'
+                    />
+                    <button>Submit</button>
+                </form>
+            </div>
         </div>
     );
 };
